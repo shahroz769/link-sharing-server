da750ed56e44a898f8083262ebb6ef96508e870a
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2023-12-05 08:12:37 +0500

    Minor cors changes

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,5 +1,9 @@
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://devvlinks.vercel.app", "https://devvlinks.netlify.app"],
+    origin: [
+        "http://localhost:5173",
+        "https://devvlinks.vercel.app",
+        "https://devslinks.vercel.app",
+    ],
     credentials: true,
     optionsSuccessStatus: 200,
 };
