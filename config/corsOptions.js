c606776ed9660777bf2014bc97688d66deaf3feb
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2024-08-03 23:06:33 +0500

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -4,6 +4,7 @@ const corsOptions = {
         "http://localhost:3000",
         "https://devvlinks.vercel.app",
         "https://devslinks.vercel.app",
+        "https://links-sharing-e0561.web.app",
     ],
     credentials: true,
     optionsSuccessStatus: 200,
